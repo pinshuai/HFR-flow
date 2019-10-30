b46272f381e6c834bf8d25c2c9c289e351a5325a
pinshuai <pshuai88@gmail.com>
2019-10-30 14:13:04 -0700

    add animation demo

---
+++ src/animation-demo.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+fig,ax = plt.subplots(1,1)
+
+x = np.arange(0, 2*np.pi, 0.1)
+y = np.sin(x)
+ax.plot(x, y)
+
+
+# add moving line
+
+# In[ ]:
+
+
+fig,ax = plt.subplots(1,1)
+
+x = np.arange(0, 2*np.pi, 0.1)
+y = np.sin(x)
+ax.plot(x, y)
+ax.plot([2,2], [-1,1])
+
+
+# ## Preplot frames
+
+# In[ ]:
+
+
+x = np.arange(0, 2*np.pi, 0.1)
+y = np.sin(x)
+
+for i in np.arange(7):
+    fig,ax = plt.subplots(1,1)
+    ax.plot(x, y)
+    
+    ax.plot([i,i], [-1,1])
+    ax.set_title(i)
+    
+    fig.savefig('./animation/sine' + str(i) + '.png')
+    plt.close(fig)
+
+
+# ### make animation using ImageMagick
+# 
+# - https://imagemagick.org/index.php
+# - https://imagemagick.org/Usage/anim_basics/
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('script', 'bash', 'convert -delay 10 -loop 0 ./animation/*.png ./animation/sine.gif')
+
+
+# ## Make animation all at once
+
+# In[ ]:
+
+
+from matplotlib.animation import FuncAnimation
+
+
+# In[ ]:
+
+
+x = np.arange(0, 2*np.pi, 0.1)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+line, = ax.plot(x, y)
+
+def update(num, x, y, line):
+    line.set_data(x[:num], y[:num])
+    line.axes.axis([0, 2*np.pi, -1, 1])
+    return line,
+
+ani = FuncAnimation(fig, func = update, frames = len(x), fargs=[x, y, line],
+                              interval=25, blit=True)
+
+ani.save('./animation/animation-sine.gif', writer='imagemagick')
+plt.show()
+
+
+# ## Using Celluloid Module
+
+# In[ ]:
+
+
+from celluloid import Camera
+
+
+# In[ ]:
+
+
+x = np.arange(0, 2*np.pi, 0.1)
+y = np.sin(x)
+
+fig,ax = plt.subplots(1,1)
+camera = Camera(fig)
+
+for i in np.arange(7):
+
+    ax.plot(x, y, 'b-')    
+    ax.plot([i,i], [-1,1], 'r-')
+#     ax.set_title(i)
+    camera.snap()
+
+animation = camera.animate()
+animation.save('./animation/celluloid_sine.gif', writer = 'imagemagick')
+
+
+# In[ ]:
+
+
+
+
